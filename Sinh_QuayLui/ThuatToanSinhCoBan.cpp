e34519e51f0150e1a211d56e0fc66b382f771784
Huyquang0519 <huyquang0519@gmail.com>
2024-05-11 10:31:10 +0700

    update

--- Sinh_QuayLui/ThuatToanSinhCoBan.cpp
+++ Sinh_QuayLui/ThuatToanSinhCoBan.cpp
@@ -41,25 +41,25 @@ int n, a[50], stop = 0;
 // }
 
 //3. Sinh xau hoan vi
-void ktao(){
-    for(int i = 1; i <= n; i++){
-        a[i] = i;
-    }
-}
+// void ktao(){
+//     for(int i = 1; i <= n; i++){
+//         a[i] = i;
+//     }
+// }
 
-void sinh(){
-    int i = n-1;
-    while(i >= 1 && a[i] > a[i+1]){
-        i--;
-    }
-    if(i == 0) stop = 1;
-    else{
-        int j = n;
-        while(a[i] > a[j]) j--;
-        swap(a[i], a[j]);
-        reverse(a+i+1, a+n+1);
-    }
-}
+// void sinh(){
+//     int i = n-1;
+//     while(i >= 1 && a[i] > a[i+1]){
+//         i--;
+//     }
+//     if(i == 0) stop = 1;
+//     else{
+//         int j = n;
+//         while(a[i] > a[j]) j--;
+//         swap(a[i], a[j]);
+//         reverse(a+i+1, a+n+1);
+//     }
+// }
 
 
 void in(){
@@ -70,11 +70,17 @@ void in(){
 }
 
 int main(){
-    cin >> n;
-    ktao();
-    while(!stop){
-        in();
-        sinh();
+    int n; cin >> n;
+    int a[n];
+    for(int i = 0; i < n; i++){
+        a[i] = i + 1;
     }
+    do{
+        for(int i = 0; i < n; i++){
+            cout << a[i] <<' ';
+        }
+        cout << endl;
+    }
+    while(next_permutation(a, a+n));
     return 0;
 }
